85553588938e814fe2ad1454d0655eeca6573ada
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-11-17 11:40:47 +0200

    Update kurssibot.py

--- kurssibot.py
+++ kurssibot.py
@@ -6,13 +6,13 @@ client = OpenAI(api_key=st.secrets["apikey"])
 
 st.title("KurssiBot")
 
-# Set a default model
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo-1106"
+MAX_CHAT_HISTORY = 1
 
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
+    # Load the system prompt only at the start of the session
+    st.session_state.messages.append({"role": "system", "content": system_prompt_content})
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
@@ -23,23 +23,23 @@ for message in st.session_state.messages:
 # Accept user input
 if prompt := st.chat_input("Kysy minulta neuvoa"):
     # Add user message to chat history
-    st.session_state.messages.append({"role": "system", "content": system_prompt_content})
     st.session_state.messages.append({"role": "user", "content": prompt})
+    
+    # Trim chat history if necessary
+    if len(st.session_state.messages) > MAX_CHAT_HISTORY:
+        st.session_state.messages = st.session_state.messages[-MAX_CHAT_HISTORY:]
 
     with st.chat_message("user"):
         st.markdown(prompt)
 
-    # Display assistant response in chat message container
+    # Display assistant response
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
         with st.spinner('puppugeneraattori aktivoitu'):
             for response in client.chat.completions.create(
                 model=st.session_state["openai_model"],
-                messages=[
-                    {"role": m["role"], "content": m["content"]}
-                    for m in st.session_state.messages
-                ],
+                messages=st.session_state.messages,
                 stream=True):
                 if response.choices[0].delta.role != "system":
                     content = response.choices[0].delta.content
@@ -47,4 +47,6 @@ if prompt := st.chat_input("Kysy minulta neuvoa"):
                         full_response += content
             message_placeholder.markdown(full_response + "▌")
             message_placeholder.markdown(full_response)
+    
+    # Append assistant's response
     st.session_state.messages.append({"role": "assistant", "content": full_response})
