bf3bb8f10a33a04b42e88e72f4b290f0952355cf
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-11-17 11:46:17 +0200

    Update kurssibot.py

--- kurssibot.py
+++ kurssibot.py
@@ -6,12 +6,14 @@ client = OpenAI(api_key=st.secrets["apikey"])
 
 st.title("KurssiBot")
 
+MAX_CHAT_HISTORY = 10  # Adjust this number as needed
+
 # Initialize the openai_model in session_state with a default value
 st.session_state.setdefault("openai_model", "gpt-3.5-turbo-1106")
 
-# Initialize chat history
+# Initialize chat history and load the system prompt only at the start of the session
 if "messages" not in st.session_state:
-    st.session_state.messages = []
+    st.session_state.messages = [{"role": "system", "content": system_prompt_content}]
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
@@ -23,10 +25,11 @@ for message in st.session_state.messages:
 if prompt := st.chat_input("Kysy minulta neuvoa"):
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
-
-    # Check and trim chat history if necessary
+    
+    # Trim chat history if necessary
     if len(st.session_state.messages) > MAX_CHAT_HISTORY:
-        st.session_state.messages = st.session_state.messages[-MAX_CHAT_HISTORY:]
+        # Keep the system prompt and the most recent messages
+        st.session_state.messages = st.session_state.messages[:1] + st.session_state.messages[-MAX_CHAT_HISTORY:]
 
     with st.chat_message("user"):
         st.markdown(prompt)
