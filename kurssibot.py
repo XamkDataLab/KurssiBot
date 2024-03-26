5e0322dc73fcfce27183683eb7d479de4b9f069e
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2024-03-26 09:28:16 +0200

    Update kurssibot.py

--- kurssibot.py
+++ kurssibot.py
@@ -6,35 +6,23 @@ client = OpenAI(api_key=st.secrets["apikey"])
 
 st.title("KurssiBot")
 
-MAX_CHAT_HISTORY = 1  # Adjust this number as needed
-
-# Initialize the openai_model in session_state with a default value
 st.session_state.setdefault("openai_model", "gpt-3.5-turbo-1106")
 
-# Initialize chat history and load the system prompt only at the start of the session
 if "messages" not in st.session_state:
     st.session_state.messages = [{"role": "system", "content": system_prompt_content2}]
 
-# Display chat messages from history on app rerun
 for message in st.session_state.messages:
     if message["role"] != "system":
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
-
-# Accept user input
-if prompt := st.chat_input("Kysy minulta neuvoa"):
-    # Add user message to chat history
+            
+prompt = st.text_area("Kysy minulta neuvoa", height=150)
+if prompt:
     st.session_state.messages.append({"role": "user", "content": prompt})
-    
-    # Trim chat history if necessary
-    if len(st.session_state.messages) > MAX_CHAT_HISTORY:
-        # Keep the system prompt and the most recent messages
-        st.session_state.messages = st.session_state.messages[:1] + st.session_state.messages[-MAX_CHAT_HISTORY:]
 
     with st.chat_message("user"):
         st.markdown(prompt)
 
-    # Display assistant response
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
@@ -50,6 +38,7 @@ if prompt := st.chat_input("Kysy minulta neuvoa"):
             message_placeholder.markdown(full_response + "▌")
             message_placeholder.markdown(full_response)
     
-    # Append assistant's response
+    
     st.session_state.messages.append({"role": "assistant", "content": full_response})
+
 
